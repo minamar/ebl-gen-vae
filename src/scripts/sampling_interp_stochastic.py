f8fda11861880b89815e8c82042266824b3bcb82
mina <mina.marmpena@gmail.com>
2019-01-09 16:06:27 +0100

    params correction

--- src/scripts/sampling_interp_stochastic.py
+++ src/scripts/sampling_interp_stochastic.py
@@ -6,9 +6,9 @@ from src.utils.visu import z_anim2dec
 from src.data.post_processing import inverse_norm
 import seaborn as sns
 sns.set(style="darkgrid")
-mina_pal = ["#000000","#004949","#009292","#ff6db6","#ffb6db","#490092","#006ddb","#b66dff","#6db6ff","#b6dbff", "#920000","#924900","#db6d00","#24ff24","#ffff6d"]
-sns.set_palette(mina_pal)
-sns.palplot(sns.color_palette())
+# mina_pal = ["#000000","#004949","#009292","#ff6db6","#ffb6db","#490092","#006ddb","#b66dff","#6db6ff","#b6dbff", "#920000","#924900","#db6d00","#24ff24","#ffff6d"]
+# sns.set_palette(mina_pal)
+# sns.palplot(sns.color_palette("colorblind", 10))
 
 """ For a specific animation or all animations within the dataset it selects a z_mean posture every interp_steps, 
     adds noise to it (same for different z), and applies interp_steps slerp interpolation between them. 
@@ -16,15 +16,17 @@ sns.palplot(sns.color_palette())
     in /visualizations.
 """
 
-check_model = '5'
+check_model = '3'
 check_epoch = '-500'
-x_dataset = 'df13_50fps.csv'
-z_mean_dataset = '5-500_df13_50fps_mean.csv'
-z_sigma_dataset = '5-500_df13_50fps_sigma.csv'
+x_dataset = 'df14_20fps.csv'
+fps = 0.05
+z_mean_dataset = '5-500_df14_20fps_mean.csv'
+z_sigma_dataset = '5-500_df14_20fps_sigma.csv'
 time_lag = 0.02
 save_stuff = True
-interp_steps = 10
+interp_steps = 30
 select = True
+dataset_dir = x_dataset.split('.')[0]
 
 # Restore model to get the decoder
 model = load_model(check_model, check_epoch)
@@ -37,15 +39,15 @@ df_z_sigma = pd.read_csv(os.path.join(ROOT_PATH, DATA_Z_PATH, z_sigma_dataset), 
 # Latent space dimensions - 2 cols for id and category
 z_dim = df_z_mean.shape[1] - 2
 
-eps = 2 + np.random.rand()
+eps = 1 + np.random.rand()
 
 # Transform latent
 df_z_mean_new = df_z_mean.copy(deep=True)
-df_z_mean_new.iloc[:, 0:z_dim] = df_z_mean.iloc[:, 0:z_dim].add(df_z_sigma.iloc[:, 0:z_dim].multiply(eps))
+df_z_mean_new.iloc[:, 0:z_dim] = df_z_mean.iloc[:, 0:z_dim].subtract(df_z_sigma.iloc[:, 0:z_dim].multiply(eps))
 
 if select:
-    category = 'Pos/Exc'
-    anim = 'Confident_1'
+    category = 'Neg/Exc' #'Pos/Exc'
+    anim = 'Fearful_1' #'Confident_1'
     # anim, category = sel_anim_id(df_anim)
     anim_id_list = [anim]
 else:
@@ -79,17 +81,20 @@ for anim_id in anim_id_list:
     scaler = 'j_scaler_nao_lim_df13_50fps.pkl'
     output_df = inverse_norm(output_df, scaler)
     # Add the time vector
-    output_df['time'] = np.linspace(time_lag, (output_df.shape[0] + 1) * 0.02, output_df.shape[0])
+    output_df['time'] = np.linspace(time_lag, (output_df.shape[0] + 1) * fps, output_df.shape[0])
     # Label for the id
     output_df['id'] = 'GEN_' + anim_id
     # Label for the category
     output_df['category'] = category
 
-    fig = z_anim2dec(df_z_mean_new, interp, output_df, anim_id, category)
+    fig = z_anim2dec(df_z_mean, interp, output_df, anim_id)
 
     if save_stuff:
         # Graphs
-        plot_path = os.path.join(ROOT_PATH, DATA_VISU, 'interp_nsteps_anim_eps', check_model + check_epoch)
+        plot_path = os.path.join(ROOT_PATH, DATA_VISU, 'interp_nsteps_anim_eps', dataset_dir)
+        if not os.path.exists(plot_path):
+            os.makedirs(plot_path)
+        plot_path = os.path.join(plot_path, check_model + check_epoch)
         if not os.path.exists(plot_path):
             os.makedirs(plot_path)
         plot_path = os.path.join(plot_path, str(interp_steps) + '_steps')
@@ -99,7 +104,10 @@ for anim_id in anim_id_list:
                     dpi=1000)
 
         # Dataframes for generated animations
-        df_path = os.path.join(ROOT_PATH, DATA_SAMP, 'interp_nsteps_anim_eps', check_model + check_epoch)
+        df_path = os.path.join(ROOT_PATH, DATA_SAMP, 'interp_nsteps_anim_eps', dataset_dir)
+        if not os.path.exists(df_path):
+            os.makedirs(df_path)
+        df_path = os.path.join(df_path, check_model + check_epoch)
         if not os.path.exists(df_path):
             os.makedirs(df_path)
         df_path = os.path.join(df_path, str(interp_steps) + '_steps')
