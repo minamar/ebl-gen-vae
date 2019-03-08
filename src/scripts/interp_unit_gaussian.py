982be8add4e0adf926599209a1de810938260b3d
mina <mina.marmpena@gmail.com>
2019-03-08 14:36:39 +0100

    use radius as std for the gaussian sampling

--- src/scripts/interp_unit_gaussian.py
+++ src/scripts/interp_unit_gaussian.py
@@ -10,48 +10,85 @@ sns.set(style="darkgrid")
 
 check_model = '42'
 check_epoch = '-200'
-method = 'spline'  # slerp, lerp, spline
+methods = ['spline']  # slerp, lerp, spline
 nsteps = 100    # per segment
 fr = 0.06
-n_pos = 100
+n_pos = 5
+std = 2  # Sampling radius
 
-# Sample a 3D diag unit gaussian
-mean = [0, 0, 0]
-cov = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
-pos_list = np.random.multivariate_normal(mean, cov, n_pos)
-pos_list = pos_list.tolist()
-
-# Get the radians frames (dec, denorm) and the latent interpolants
-df_dec_interp, df_z_interp = interp_multi(pos_list, True, nsteps, check_model, check_epoch, method)
-
-# Add 'time' column based on frequency fr
-end = df_dec_interp.shape[0] * fr + 0.02
-df_dec_interp['time'] = list(np.arange(0.02, end, fr))
 
 # Save path
 df_path = os.path.join(ROOT_PATH, DATA_SAMP, 'interp_unit_gaussian')
 
-# Prepare the overview
-json_file = os.path.join(df_path, '-overview.json')
+# Sample a 3D diag unit gaussian
 
-with open(json_file, 'r') as fd:
-    files_dict = json.load(fd)
+mean = [0, 0, 0]
+cov = [[std, 0, 0], [0, std, 0], [0, 0, std]]
+pos_list = np.random.multivariate_normal(mean, cov, n_pos)
+pos_list = pos_list.tolist()
 
-file_id = len(files_dict)
 
-files_dict[file_id] = {
-    'file_id': file_id,
-    'interp_method': method,
-    'interp_steps': nsteps,
-    'frequency': fr,
-    'model': check_model + check_epoch,
-    'gauss_samples': pos_list
-}
+# =======
+# import numpy as np
+#
+# def sample_spherical(npoints, ndim=3):
+#     vec = np.random.randn(ndim, npoints)
+#     vec /= np.linalg.norm(vec, axis=0)
+#     return vec
+#
+# from matplotlib import pyplot as plt
+# from mpl_toolkits.mplot3d import axes3d
+# r = 6
+# phi = np.linspace(0, np.pi, 10)
+# theta = np.linspace(0, 2 * np.pi, 20)
+# x = r * np.outer(np.sin(theta), np.cos(phi))
+# y = r * np.outer(np.sin(theta), np.sin(phi))
+# z = r * np.outer(np.cos(theta), np.ones_like(phi))
+#
+# xi, yi, zi = sample_spherical(10)
+#
+#
+# fig, ax = plt.subplots(1, 1, subplot_kw={'projection':'3d', 'aspect':'equal'})
+# # ax.scatter(x, y, z)
+# ax.plot_wireframe(x, y, z, color='k', rstride=1, cstride=1)
+# # ax.scatter(xi, yi, zi, s=100, c='r', zorder=10)
+# ax.set_xlabel('x')
+# ax.set_ylabel('y')
+# ax.set_zlabel('z')
+#
+# plt.show()
+# pos_list = [list(i) for i in zip(x.flatten('F'),y.flatten('F'),z.flatten('F'))]
+# ==========
 
-with open(json_file, 'w') as fd:
-    fd.write(json.dumps(files_dict))
+for method in methods:
+    # Get the radians frames (dec, denorm) and the latent interpolants
+    df_dec_interp, df_z_interp = interp_multi(pos_list, True, nsteps, check_model, check_epoch, method)
 
-# Save
-df_dec_interp.to_csv(os.path.join(df_path, str(file_id) + '_dec_' + method + '.csv'))
-df_z_interp.to_csv(os.path.join(df_path, str(file_id) + '_z_' + method + '.csv'))
+    # Add 'time' column based on frequency fr
+    end = df_dec_interp.shape[0] * fr + 0.02
+    df_dec_interp['time'] = list(np.arange(0.02, end, fr))
+
+    # Prepare the overview
+    json_file = os.path.join(df_path, '-overview.json')
+
+    with open(json_file, 'r') as fd:
+        files_dict = json.load(fd)
+
+    file_id = len(files_dict)
+
+    files_dict[file_id] = {
+        'file_id': file_id,
+        'interp_method': method,
+        'interp_steps': nsteps,
+        'frequency': fr,
+        'model': check_model + check_epoch,
+        'sampling radius': std
+    }
+
+    with open(json_file, 'w') as fd:
+        fd.write(json.dumps(files_dict))
+
+    # Save
+    df_dec_interp.to_csv(os.path.join(df_path, str(file_id) + '_dec_' + method + '.csv'))
+    df_z_interp.to_csv(os.path.join(df_path, str(file_id) + '_z_' + method + '.csv'))
 
