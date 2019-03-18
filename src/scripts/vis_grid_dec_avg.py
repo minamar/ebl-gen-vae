c393d37fb1ccb55b7029d947d4a07a96034f8b57
mina <mina.marmpena@gmail.com>
2019-03-18 13:24:07 +0100

    handle data interp_grid_longintude

--- src/scripts/vis_grid_dec_avg.py
+++ src/scripts/vis_grid_dec_avg.py
@@ -18,44 +18,49 @@ sns.set(style="whitegrid")
 check_model = '42'
 check_epoch = '-200'
 mode = 'avg'
-
+lat_dim = ['l1', 'l2', 'l3']
 # Directory with sampled anims
 gen_vae_dir = 'interp_grid_longitude'
 
-re1 = r'(1[0-1]|[0-9])_(dec)'
-re2 = r'(1[2-9]|2[0-3])_(dec)'
-re3 = r'(2[4-9]|3[0-5])_(dec)'
-re_list = [re1, re2, re3]
-
-path_to_folder = os.path.join(ROOT_PATH, DATA_SAMP, gen_vae_dir)
-with open(path_to_folder + '/-overview.json', 'r') as f:
-    details = json.load(f)
-# Plot settings
+# 3 plots, one for ealatent dim
 set_pub()
 fig, ax = plt.subplots(3, sharex=True, figsize=(30, 18))
 
-for i in range(3):
-    # itemList = os.listdir(path_to_folder)
-    x_dataset = [f for f in os.listdir(path_to_folder) if re.match(re_list[i], f)]
-    # All in radians, decoded, normalized
+for l in range(3):
+    path_to_folder = os.path.join(ROOT_PATH, DATA_SAMP, gen_vae_dir, lat_dim[l])
+    with open(path_to_folder + '/-overview.json', 'r') as f:
+        details = json.load(f)
 
+# re1 = r'(1[0-1]|[0-9])_(dec)'
+# re2 = r'(1[2-9]|2[0-3])_(dec)'
+# re3 = r'(2[4-9]|3[0-5])_(dec)'
+# re_list = [re1, re2, re3]
+
+    re1 = r'\w+(dec)+\w'
+
+    filenames = [f for f in os.listdir(path_to_folder) if re.match(re1, f)]
+    # All in radians, decoded, normalized
+    x_dataset = sorted(filenames, key=lambda item: (int(item.partition('_')[0])
+                                                    if item[0].isdigit() else float('inf'), item))
     df = pd.DataFrame()
     for data in x_dataset:
         label = data.split('_')[0]
         radius = details[label]['grid_radius']
-        df_data = pd.read_csv(os.path.join(ROOT_PATH, DATA_SAMP, gen_vae_dir, data), index_col=0)
+        longitude = details[label]['longitude']
+        df_data = pd.read_csv(os.path.join(ROOT_PATH, DATA_SAMP, gen_vae_dir, lat_dim[l], data), index_col=0)
         df_data.drop(columns='time', inplace=True)
-        # df_data['set'] = label
         df_data['radius'] = radius
+        # df_data['longitude'] = longitude
 
         df = pd.concat([df, df_data], axis=0)
 
-    df = df.pivot(columns='radius')
+    df.reset_index(drop=True, inplace=True)
+    df = df.pivot(columns='radius', values=joints_names)
 
     df_mean = df.mean(axis=1, level=1)
     df_mean.columns = df_mean.columns.rename('')
-    df_plot = df_mean # .loc[0:500]
-    df_plot.plot(sort_columns=True, colormap='gist_heat', ax=ax[i], yticks=np.arange(-0.4, 0.6, 0.2), xticks=np.arange(0, df_plot.shape[0], 100), legend=False)
+    df_plot = df_mean
+    df_plot.plot(sort_columns=True, colormap='gist_heat', ax=ax[l], yticks=np.arange(-0.4, 0.6, 0.2), xticks=np.arange(0, df_plot.shape[0], 100), legend=False)
 
 ax[2].set(xlabel="frames")
 ax[1].set(ylabel="mean joint angles (radians)")
