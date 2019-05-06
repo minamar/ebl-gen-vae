72ec8eff7ec3d0a75b8b0c93d794d2657cf24e98
mina <mina.marmpena@gmail.com>
2019-05-06 10:27:06 +0200

    params

--- src/scripts/train_Cvae.py
+++ src/scripts/train_Cvae.py
@@ -16,11 +16,11 @@ subset = None  # from settings pos_neu_ids
 # 'Emotions/Positive'
 
 lr = 0.0001
-latent_range = [3]
+latent_range = [5]
 batch = 32
 encoder = [128, 512, 128]
 decoder = [128, 512, 128]
-n_epoch = 501
+n_epoch = 201
 wu = False  # Warm-up
 beta = 0.001
 beta_range = np.linspace(0.0001, 0.01, n_epoch)
