38da52348e33cf7e1ad01a7d83623c592cfbd2a8
mina <mina.marmpena@gmail.com>
2019-05-16 14:30:57 +0200

    clean up

--- src/scripts/cond_torus_longitude.py
+++ src/scripts/cond_torus_longitude.py
@@ -1,26 +1,29 @@
 import os
 import json
 from settings import *
-from src.utils.sampu import interp_multi
+from src.utils.sampu import interp_multi, v2cat_value
+import math
 
 """ Sample longitude lines from the spherical grid. Only for 3D latent space """
 
-check_model = '60'
-check_epoch = '-100'
+check_model = '63'
+check_epoch = '-250'
 feats_names = joints_names + leds_keys
-method = 'slerp'
-nsteps = 10    # per segment
+method = 'spline'
+nsteps = 20    # per segment
 fr = 0.06
-radiuses = [0.5, 1, 1.5, 2, 2.5, 3]  # Sampling radius
-va_list = [[0, 0], [0.5, 0.5], [1, 1], [0, 0.5], [0, 1], [0.5, 0], [1, 0], [1, 0.5], [0.5, 1]]
+radiuses = [0.5, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5]  # Sampling radius
+# av_list = [[0, 0], [0.5, 0.5], [1, 1], [0, 0.5], [0, 1], [0.5, 0], [1, 0], [1, 0.5], [0.5, 1]]
+av_list = [[0.], 1], [0.5], [0.6], [0.9], [1.]]
+
 lat = 20    # Points on a circe (first and last are the same)
 long = 9
 
-# TODO: add param for the model id
 # Save path
-df_path = os.path.join(ROOT_PATH, DATA_SAMP, 'cond_torus_longitude/60-100')
+df_path = os.path.join(ROOT_PATH, DATA_SAMP, 'cond_torus_longitude/' + check_model + check_epoch + '/l3')
 
 for radius in radiuses:
+    nsteps = math.ceil(5 * radius)
     R = radius/2
     r = radius/2
     theta_t = np.linspace(0, 2 * np.pi, lat)
@@ -35,10 +38,10 @@ for radius in radiuses:
         pos_list = [list(i) for i in zip(x[c, :], y[c, :], z[c, :])]
         longitude = c
 
-        for va in va_list:
+        for av in av_list:
             K=None
             # Get the radians frames (dec, denorm) and the latent interpolants
-            df_dec_interp, df_z_interp = interp_multi(pos_list, True, nsteps, check_model, check_epoch, method, feats_names, cond_VA=va)
+            df_dec_interp, df_z_interp = interp_multi(pos_list, True, nsteps, check_model, check_epoch, method, feats_names, cond_AV=av)
 
             # Add 'time' column based on frequency fr
             end = df_dec_interp.shape[0] * fr + 0.02
@@ -51,6 +54,8 @@ for radius in radiuses:
                 files_dict = json.load(fd)
 
             file_id = len(files_dict)
+            category = v2cat_value(av[0])
+
 
             files_dict[file_id] = {
                 'file_id': file_id,
@@ -61,8 +66,8 @@ for radius in radiuses:
                 'grid_radius': radius,
                 'longitude': longitude,
                 'latent_points': lat,
-                'VA_score': va,
-                'VA_cat': va2cat[str(va)]
+                'AV_score': av,
+                'AV_cat': category
 
             }
 
@@ -71,6 +76,6 @@ for radius in radiuses:
 
             # Save
             df_dec_interp.to_csv(
-                os.path.join(df_path, str(file_id) + '_dec_long' + str(longitude) + '_' + 'r' + str(radius) + '_' +  va2cat[str(va)] + '.csv'))
+                os.path.join(df_path, str(file_id) + '_dec_long' + str(longitude) + '_' + 'r' + str(radius) + '_' + category + '.csv'))
             df_z_interp.to_csv(
-                os.path.join(df_path, str(file_id) + '_z_long' + str(longitude) + '_' + 'r' + str(radius) + '_' +  va2cat[str(va)] + '.csv'))
+                os.path.join(df_path, str(file_id) + '_z_long' + str(longitude) + '_' + 'r' + str(radius) + '_' + category + '.csv'))
--- src/scripts/vis_torus.py
+++ src/scripts/vis_torus.py
@@ -15,28 +15,30 @@ theta_t, phi_t = np.meshgrid(theta_t, phi_t)
 R = 1.5
 
 r = 1.5
-x = (R + r*np.cos(theta_t)) * np.cos(phi_t)
-y = (R + r*np.cos(theta_t)) * np.sin(phi_t)
+x = (R + r * np.cos(theta_t)) * np.cos(phi_t)
+y = (R + r * np.cos(theta_t)) * np.sin(phi_t)
 z = r * np.sin(theta_t)
 
 fig4 = plt.figure()
 ax = plt.axes(projection='3d')
 
 # ax1.plot_surface(x, y, z, rstride=5, cstride=5, edgecolors='k')
-ax.plot(x.flatten('C'), y.flatten('C'), z.flatten('C'))
+ax.scatter(x.flatten('C'), y.flatten('C'), z.flatten('C'))
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+ax.set_zlabel('z')
 
-
-
-radius = 3
-circles = 6  # Parallel to z axis. Equal to x2 longitudes (circle)
-lat = 41    # Points on a circe (first and last are the same)
-
-phi = np.linspace(0, np.pi, circles)  # 10 times the perimeter, parallel to z axis (longitude)
-theta = np.linspace(0, 2 * np.pi, lat)  # (latidude) in parallel to x,y plane
-x = radius * np.outer(np.sin(theta), np.cos(phi))  # the plane
-y = radius * np.outer(np.sin(theta), np.sin(phi))  # the plane
-z = radius * np.outer(np.cos(theta), np.ones_like(phi))  # the axis
-ax.plot(x.flatten('F'), y.flatten('F'), z.flatten('F'))
+#
+# radius = 3
+# circles = 6  # Parallel to z axis. Equal to x2 longitudes (circle)
+# lat = 41    # Points on a circe (first and last are the same)
+#
+# phi = np.linspace(0, np.pi, circles)  # 10 times the perimeter, parallel to z axis (longitude)
+# theta = np.linspace(0, 2 * np.pi, lat)  # (latidude) in parallel to x,y plane
+# x = radius * np.outer(np.sin(theta), np.cos(phi))  # the plane
+# y = radius * np.outer(np.sin(theta), np.sin(phi))  # the plane
+# z = radius * np.outer(np.cos(theta), np.ones_like(phi))  # the axis
+# ax.plot(x.flatten('F'), y.flatten('F'), z.flatten('F'))
 
 # radius = 0.5
 # phi = np.linspace(0, np.pi, circles)  # 10 times the perimeter, parallel to z axis (longitude)
