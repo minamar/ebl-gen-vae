1ec832b6b93a1eb09f3472ca4978daa15c4c0bdf
minamar <mina.marmpena@gmail.com>
2020-10-13 16:48:17 +0200

    doc

--- src/data/augment_mirroring.py
+++ src/data/augment_mirroring.py
@@ -4,25 +4,26 @@ import os
 from settings import *
 
 """
-Augment the original training examples by adding anti-symmetrical ones. Doubles the quantity of examples. 
-The new examples have reversed sign for the joint values of 'HeadYaw', 'HipRoll', swapped values for Arms 
-(RArm to LArm and vice versa).   
+Augment the original training examples by adding anti-symmetrical ones in terms of left/right motion. 
+Doubles the quantity of examples. The new examples have reversed sign for the joint values of 
+'HeadYaw', 'HipRoll', swapped values for Arms (RArm to LArm and vice versa).   
 """
 
-# Original keyframes and destination file to write normalized keyframes
+# Original keyframes and destination file to write
 data_x_set = 'df3_25fps.csv'
 dest_x_set = 'df31_25fps.csv'
 
 data_y_set = 'y_va_cat.csv'
 dest_y_set = 'y_va_cat_aug.csv'
 
+# If True, valence and arousal labels are also augmented
 augment_labels = False
 
 # Augment the training examples first
 path = os.path.join(ROOT_PATH, RAW_DATA, data_x_set)
 df = pd.read_csv(path, index_col=0)
 
-# Reverse sign for HeadYaw and HipRoll
+# Reverse the sign of HeadYaw and HipRoll values
 df_tr = df.copy(deep=True)
 df_tr.loc[:, ['HeadYaw', 'HipRoll']] = -df_tr.loc[:, ['HeadYaw', 'HipRoll']]
 
@@ -32,20 +33,18 @@ swap_r  = df_tr.loc[:, ['RShoulderPitch', 'RShoulderRoll', 'RElbowRoll', 'RElbow
 df_tr[['LShoulderPitch', 'LShoulderRoll', 'LElbowRoll', 'LElbowYaw', 'LWristYaw', 'LHand']] = swap_r
 df_tr[['RShoulderPitch', 'RShoulderRoll', 'RElbowRoll', 'RElbowYaw', 'RWristYaw', 'RHand']] = swap_l
 
-# Inverse signs for for arms roll and yaw joints
+# Inverse signs for arms roll and yaw joints
 inv_joints = ['LShoulderRoll', 'LElbowRoll', 'LElbowYaw', 'LWristYaw', 'RShoulderRoll', 'RElbowRoll', 'RElbowYaw', 'RWristYaw']
 df_tr.loc[:, inv_joints] = -df_tr.loc[:, inv_joints]
 
-# Change the anim id
-df_tr['id'] = df_tr['id'] + '_tr
+# Add a '_tr' suffix (stands for 'transformed') to the anim id
+df_tr['id'] = df_tr['id'] + '_tr'
 
 # Add to previous dataframe
 augm_df = pd.concat([df, df_tr], ignore_index=True)
 
 dest = os.path.join(ROOT_PATH, DATA_X_PATH, dest_x_set)
 augm_df.to_csv(dest)
-
-
 
 # Augment the labels
 if augment_labels:
