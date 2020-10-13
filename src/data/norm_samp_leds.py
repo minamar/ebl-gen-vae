03801bc092f7ccc9a0cb37016032cc8d6f9778c2
minamar <mina.marmpena@gmail.com>
2020-10-13 16:56:54 +0200

    doc

--- src/data/norm_samp_leds.py
+++ src/data/norm_samp_leds.py
@@ -7,15 +7,16 @@ from settings import *
 
 """
 For dataframes with motion + LEDs. Normalizes the joint values to (0,1).
-Ignores the time and the leds columns. 
+Ignores the time and the LEDs columns. 
 """
 
 # Original keyframes and destination file to write normalized keyframes
 x_set = 'df31_25fps.csv'
 dest_x_set = 'df32_25fps.csv'
-scaler_file = 'j_scaler_nao_lim_df32_25fps.pkl'  # Change df number with the one of dest_x_set, and whether it is a 'ds' or 'nao'
-
-lims = 'naoqi'  # 'naoqi' to normalize with the full range naoqi limits or 'dataset' to normalize with the dataset range
+# Change df number with the one of dest_x_set, and whether it is a 'ds' or 'nao'
+scaler_file = 'j_scaler_nao_lim_df32_25fps.pkl'
+# 'naoqi' to normalize with the full range naoqi limits or 'dataset' to normalize with the dataset range
+lims = 'naoqi'
 
 dest_scaler = os.path.join(ROOT_PATH, SCALERS_PATH, scaler_file)
 
@@ -31,14 +32,10 @@ if lims == 'naoqi':
     df_joints.loc[len(df_joints)] = list(joints_minmax[:, 0])
     df_joints.loc[len(df_joints)] = list(joints_minmax[:, 1])
 
-# Train the normalization
+# Normalize the dataset
 scaler = MinMaxScaler(feature_range=(0, 1))
 scaler = scaler.fit(df_joints)
-
-# Normalize the dataset
 normalized = scaler.transform(df_joints)
-
-# Save the scaler for inversing output values later
 joblib.dump(scaler, dest_scaler)
 
 # Normalizes with naoqi full range limits (joints_minmax in settings.py)
